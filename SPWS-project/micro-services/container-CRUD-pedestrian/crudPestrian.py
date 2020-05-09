107fb5955ad41f7fa236d713b81a8979d147bea7
josepereira1 <joseandremp1@gmail.com>
2020-05-09 14:46:49 +0100

    crud pedestrian micro service functional with redis

--- SPWS-project/micro-services/container-CRUD-pedestrian/crudPestrian.py
+++ SPWS-project/micro-services/container-CRUD-pedestrian/crudPestrian.py
@@ -4,22 +4,33 @@ import redis
 
 app = Flask(__name__)
 
+r = redis.Redis(host='redis', port=6379)
+
 @app.route("/updateLocation", methods=['POST'])
-def updateLocation():   
+def updateLocation():
     if('id' in request.json and 'latitude' in request.json and 'longitude' in request.json and 'elevation' in request.json):
         id = request.json.get('id')
         latitude = request.json.get('latitude')
         longitude = request.json.get('longitude')
         elevation = request.json.get('elevation')
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        
+
         json =  '{ "id":' + str(id)+ ',"latitude":' + str(latitude) + ',"longitude":' + str(longitude) + ',"elevation":' + str(elevation) + '}'
 
         r.set(id, json)
-        
+
         return "ok"
     else: return "ko"
 
+@app.route("/getLocation", methods=["POST"])
+def getLocation():
+    if('id' in request.json and r.exists(request.json.get('id'))):
+        return r.get(request.json.get('id'))
+
+    return "ko"
+
+@app.route("/", methods=['GET', 'POST'])
+def root():
+        return "service ready"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5003", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
