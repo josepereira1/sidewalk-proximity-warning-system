5de2218432e56d6435f30e977e3ffb3e5f50083b
josepereira1 <joseandremp1@gmail.com>
2020-06-11 15:07:21 +0100

    documentação

--- dev/spws/micro-services/container-closest-crosswalk/closestCrosswalk.py
+++ dev/spws/micro-services/container-closest-crosswalk/closestCrosswalk.py
@@ -28,6 +28,9 @@ while True:
 app = Flask(__name__)
 CORS(app) # enables CORS support on all routes, for all origins and methods
 
+#   inicializa o redis deste micro serviço, buscando a informação das crosswalks ao micro serviço "crud-crosswalk-location"
+#   no input
+#   output (TEXT): "redis loaded"
 @app.route("/initRedis", methods=['GET', 'POST'])
 def initRedis():
     url = "crud-crosswalk-location"    
@@ -38,16 +41,19 @@ def initRedis():
         r.set(crosswalk['id'], json.dumps(crosswalk))
     return "redis loaded"
 
-
+#   determina a crosswalk mais próxima da entidade (pedestrian ou vehicle), no entanto, APENAS retorna se estiver no interior do raio da vizinhança
+#   input (JSON): {"id": 1, "latitude": 1, "longitude": 1, "elevation": 1}
+#   output (JSON): {"user_id": 1, "latitude": 1, "longitude": 1, "elevation": 1, "crosswalk_id": 1, "distance": 0.00001}
 def closestCrosswalk(ch, method, properties, body):
     distances = {}
 
     user = json.loads(body) # python object
-
+    
     keys = r.keys()
     for key in keys:
         crosswalk = r.get(key) # json string
         crosswalk = json.loads(crosswalk) # python object
+        #   fórmula da distância
         distances[key] = math.sqrt( ((user['latitude']-crosswalk['latitude'])**2)+((user['longitude']-crosswalk['longitude'])**2)+((user['elevation']-crosswalk['elevation'])**2) ) 
 
     id_closest_crosswalk = min(distances, key=distances.get)
@@ -55,7 +61,7 @@ def closestCrosswalk(ch, method, properties, body):
     # 0.0001 <=> 11 m
     # 0.0002 <=> 22 m
     # ...
-    #   TODO MUDAR PARA 0.0001
+    #   testar se a entidade está no interior do raio, neste caso 0.0001, que é aproximadamente 11 metros
     if distances[id_closest_crosswalk] < 0.0001: 
         sender = Sender('rabbitmq-closest-crosswalk')
         sender.setQueue('output')
@@ -66,6 +72,7 @@ def closestCrosswalk(ch, method, properties, body):
 thread = Thread( target = receiver.setQueue, args = ('input', closestCrosswalk) )
 thread.start()
 
+#   atualiza/adiciona a informação de uma crosswalk, usado nas propagações das escritas no serviço que guarda esta informação
 @app.route("/updateCrosswalk", methods=['POST'])
 def updateCrosswalk():
     if 'id' in request.json and 'latitude' in request.json and 'longitude' in request.json and 'elevation' in request.json:
