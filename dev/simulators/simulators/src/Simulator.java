4438d1ee890ed589bffa6cfd009bf96f9edcdc53
RicardoPetronilho98 <rpetronilho98@gmail.com>
2020-06-11 15:33:08 +0100

    versão final v1.0

--- dev/simulators/simulators/src/Simulator.java
+++ dev/simulators/simulators/src/Simulator.java
@@ -4,6 +4,8 @@ public class Simulator {
     public static final String SPWS_URL = "http://localhost:5007/closestCrosswalk";
 
     public static void main(String[] args) throws Exception {
-        Device.parseDevices("devices/").forEach(Thread::start);
+        Device.parseDevices("devices/car-x.json").forEach(Thread::start);
+        //Device.parseDevices("devices/pedestrian-x.json").forEach(Thread::start);
+        //Device.parseDevices("devices/").forEach(Thread::start);
     }
 }
