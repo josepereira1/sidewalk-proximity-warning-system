522b1b67daab673c43849eb1626cda9e82efdb21
RicardoPetronilho98 <rpetronilho98@gmail.com>
2020-06-11 13:36:14 +0100

    removi o micro serviço calculate-distance e documentei o código.

--- dev/spws/micro-services/container-CRUD-crosswalk-counters/crudCrosswalkCounters.py
+++ dev/spws/micro-services/container-CRUD-crosswalk-counters/crudCrosswalkCounters.py
@@ -4,6 +4,11 @@ import redis
 import json
 import datetime
 
+# após 5 segundos elemina a associação do user à passadeira, assim caso o user
+# se desconecte do sistema a meio de uma passadeira, os seus dados são ignorados
+# passado 5 segundos uma vez que seria falso alarme contar com os mesmos
+TTL = 5 
+
 r = redis.Redis(host='redis-crud-crosswalk-counters', port=6379, charset="utf-8", decode_responses=True)
 
 # inicia o servidor
@@ -11,6 +16,12 @@ app = Flask(__name__)
 CORS(app) # enables CORS support on all routes, for all origins and methods
 
 
+# input (JSON): { "user_id": "p0", "crosswalk_id": 0 }
+# ouput (TEXT): yes
+# ouput (TEXT): no
+# ouput (TEXT): ko
+# incrementa os contadores de pedestres e veículos (tanto o histórico como o atual)
+# verifica se há perigo isto é, se existem users de tipos opostos
 @app.route("/updateInfo", methods=['POST'])
 def updateInfo():
     if 'user_id' in request.json and 'crosswalk_id' in request.json:
@@ -18,103 +29,101 @@ def updateInfo():
         user_id = str(request.json['user_id'])
         crosswalk_id = str(request.json['crosswalk_id'])
 
-        # incrementa os contadores dos pedestres ou veículos na passadeira
+        # incrementa os contadores dos pedestres (tanto o histórico como o atual)
         if user_id[0] == "p": 
             if not r.exists("u" + user_id + crosswalk_id):
-                r.incr("p" + crosswalk_id)
-                r.incr("hp" + crosswalk_id) #   histórico
-                r.set("u" + user_id + crosswalk_id, "true" )
-                r.sadd("c" + crosswalk_id, user_id) 
+                r.incr("p" + crosswalk_id) # contador atual
+                r.incr("hp" + crosswalk_id) # contador do histórico
+                r.set("u" + user_id + crosswalk_id, "true" ) # indica qual a passadeira que o user se econtra
+                r.sadd("c" + crosswalk_id, user_id)  # indica qual os users na passadeira
+        # incrementa os contadores dos veículos (tanto o histórico como o atual)
         elif user_id[0] == "v":
             if not r.exists("u" + user_id + crosswalk_id):
-                r.incr("v" + crosswalk_id)
-                r.incr("hv" + crosswalk_id) #   histórico
-                r.set("u" + user_id + crosswalk_id, "true" )
-                r.sadd("c" + crosswalk_id, user_id) 
+                r.incr("v" + crosswalk_id)  # contador atual
+                r.incr("hv" + crosswalk_id) # contador do histórico
+                r.set("u" + user_id + crosswalk_id, "true" ) # indica qual a passadeira que o user se econtra
+                r.sadd("c" + crosswalk_id, user_id) # indica qual os users na passadeira
+        # internal server error (é suposto isto não acontecer)
         else: return "ko"
 
-        # renova o TTL
-        r.expire("u" + user_id + crosswalk_id, 5)
+        # renova o TTL da associação do user à passadeira
+        r.expire("u" + user_id + crosswalk_id, TTL)
 
         # ------------------- VERIFICAR SE HÁ PERIGO -------------------
 
-        crosswalk_id = str(request.json['crosswalk_id'])
-        user_id_input = str(request.json['user_id'])
-
-        crosswalk_users_ids = r.smembers("c" + crosswalk_id) # python object
+        # set com os ids dos users que estão na crosswalk
+        crosswalk_users_ids = r.smembers("c" + crosswalk_id)
 
         for crosswalk_user_id  in crosswalk_users_ids:
-            if r.exists("u" + user_id_input + crosswalk_id) and user_id_input[0] == 'p' and crosswalk_user_id[0] == 'v':
-                return "yes"    #   há perigo
-            if r.exists("u" + user_id_input + crosswalk_id) and user_id_input[0] == 'v' and crosswalk_user_id[0] == 'p':
-                return "yes"    #   há perigo
-        return "no"             #   não há perigo   
+            if r.exists("u" + user_id + crosswalk_id) and user_id[0] == 'p' and crosswalk_user_id[0] == 'v':
+                return "yes" # há perigo
+            if r.exists("u" + user_id + crosswalk_id) and user_id[0] == 'v' and crosswalk_user_id[0] == 'p':
+                return "yes" # há perigo
+        
+        # percorreu todos os users e não encontrou tipos opostos por isso não há perigo
+        return "no"
+    
     else:
-        return "ko"
+        return "ko" # internal server error (é suposto isto não acontecer)
 
 
+# input (JSON): { "crosswalk_id": 0 }
+# ouput (JSON): {"crosswalk_id": 0, "history_npedestrians": 100, "npedestrians": 20, "history_nvehicles": 50, "nvehicles": 30, "users_ids": ["v0", "p0", "p1", "p2"] }        
+# ouput (TEXT): ko
+# obtém os contadores de pedestres e veículos (tanto o histórico como o atual)
+# verifica se há perigo (isto é se existem users de tipos opostos)
 @app.route("/getInfo", methods=["POST"])
 def getInfo():
     if 'crosswalk_id' in request.json:
         
         crosswalk_id = str(request.json['crosswalk_id'])
     	
-        # obtém os contadores
+        # obtém o contador dos pedestres atual
         if r.exists("p" + crosswalk_id): npedestrians = int(r.get("p" + crosswalk_id))
         else: npedestrians = 0
       
+        # obtém o contador dos veículos atual
         if r.exists("v" + crosswalk_id): nvehicles = int(r.get("v" + crosswalk_id))
         else: nvehicles = 0
 
+        # obtém o contador dos pedestres do histórico
         if r.exists("hp" + crosswalk_id): history_npedestrians = int(r.get("hp" + crosswalk_id))
         else: history_npedestrians = 0
 
+        # obtém o contador dos veículos do histórico
         if r.exists("hv" + crosswalk_id): history_nvehicles = int(r.get("hv" + crosswalk_id))
         else: history_nvehicles = 0
 
-        # obtém a lista de users
-        users_ids = r.smembers("c" + crosswalk_id) # python object
+        # obtém a lista de users naquela passadeira
+        users_ids = r.smembers("c" + crosswalk_id)
 
         # remove os users que tenham atingido o TTL
         for user_id in users_ids:
             if not r.exists("u" + user_id + crosswalk_id):
-                r.srem("c" + crosswalk_id, user_id) # remove o user da passadeira
+                r.srem("c" + crosswalk_id, user_id) # remove a associação do user à passadeira
 
         # obtém novamente a lista de users já atualizada
-        users_ids = r.smembers("c" + crosswalk_id) # python object
+        users_ids = r.smembers("c" + crosswalk_id)
        
-        # convert to json
+        # manualy convert set to json (because set is not serializable)
         if len(users_ids) == 0: res = "[]"
         else: 
             res = "["
             for user_id in users_ids: 
                 res += '"' + user_id + '", '
-            res = res[:-2]
+            res = res[:-2] # tirar os últimos 2 caracteres (vírugla e espaço)
             res += "]"
 
-        res = '{"crosswalk_id":' + crosswalk_id + ', "history_npedestrians":' + str(history_npedestrians) + ', "npedestrians":' + str(npedestrians) +  ', "history_nvehicles":' + str(history_nvehicles) + ', "nvehicles":' + str(nvehicles) + ', "users_ids":' + res + '}'
-        
+        res = '{"crosswalk_id":' + crosswalk_id + ', "history_npedestrians":' + str(history_npedestrians) + ', "npedestrians":' + str(npedestrians) +  ', "history_nvehicles":' + str(history_nvehicles) + ', "nvehicles":' + str(nvehicles) + ', "users_ids":' + res + '}'        
         return res
-    return "ko"
-
-@app.route("/hasDangerous", methods=['POST'])
-def hasDangerous():
-    if 'user_id' in request.json and 'crosswalk_id' in request.json:
-        crosswalk_id = str(request.json['crosswalk_id'])
-        user_id_input = str(request.json['user_id'])
-
-        crosswalk_users_ids = r.smembers("c" + crosswalk_id) # python object
-
-        for crosswalk_user_id  in crosswalk_users_ids:
-            if r.exists("u" + user_id_input + crosswalk_id) and user_id_input[0] == 'p' and crosswalk_user_id[0] == 'v':
-                return "yes"
-            if r.exists("u" + user_id_input + crosswalk_id) and user_id_input[0] == 'v' and crosswalk_user_id[0] == 'p':
-                return "yes"
-        return "no"
+    
     else:
-        return "ko"
+        return "ko" # internal server error (é suposto isto não acontecer)
 
 
+# input (JSON): { "crosswalk_id": 0 }
+# ouput (TEXT): ok
+# ouput (TEXT): ko
 @app.route("/resetDailyCountersCrosswalk", methods=["POST"])
 def resetCountersCrosswalk():
     if 'crosswalk_id' in request.json:
@@ -124,11 +133,13 @@ def resetCountersCrosswalk():
         if r.exists("p" + crosswalk_id):
             r.set("p" + crosswalk_id, 0)
         return "ok"
-    else: return "ko"
+    else: return "ko" # internal server error (é suposto isto não acontecer)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def root():
         return "service is ready"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5004", debug=True)
--- dev/spws/micro-services/container-CRUD-pedestrian/crudPedestrian.py
+++ dev/spws/micro-services/container-CRUD-pedestrian/crudPedestrian.py
@@ -3,17 +3,27 @@ from flask_cors import CORS
 import json
 import redis
 
+# após 5 segundos elemina a localização do user, assim caso o user
+# se desconecte do sistema a meio de uma passadeira, os seus dados são ignorados
+# passado 5 segundos uma vez que seria falso alarme contar com os mesmos
+TTL = 5 
+
 r = redis.Redis(host='redis-crud-pedestrian', port=6379, charset="utf-8", decode_responses=True)
 
 # inicia o servidor
 app = Flask(__name__)
 CORS(app) # enables CORS support on all routes, for all origins and methods
 
+
+# input (JSON): { "id": "p0", "latitude": 1, "longitude": 2, "elevation": 3, "distance": 5.25 }
+# ouput (TEXT): ok 
+# ouput (TEXT): ko
 # UPDATE/ CREATE
+# atualiza/ cria a localização de um pedestre
 @app.route("/updateLocation", methods=['POST'])
 def updateLocation():
-    
-    if('id' in request.json and 'latitude' in request.json and 'longitude' in request.json and 'elevation' in request.json and 'distance' in request.json):
+    if 'id' in request.json and 'latitude' in request.json and 'longitude' in request.json and 'elevation' in request.json and 'distance' in request.json:
+        
         id = str(request.json.get('id'))
         latitude = request.json.get('latitude')
         longitude = request.json.get('longitude')
@@ -21,50 +31,37 @@ def updateLocation():
         distance = request.json.get('distance')
 
         userLocation =  '{ "id":"' + id + '","latitude":' + str(latitude) + ',"longitude":' + str(longitude) + ',"elevation":' + str(elevation) + ',"distance":' + str(distance) +'}'
-
-        r.set(id, userLocation)
-
+        r.set(id, userLocation, TTL)
         return "ok"
-    else: return "ko"
-
-@app.route("/getLocation", methods=["POST"])
-def getLocation():
-    if('id' in request.json and r.exists(request.json.get('id'))):
-        return r.get(request.json.get('id'))
     else: 
-        return "ko"
+        return "ko" # internal server error (é suposto isto não acontecer)
 
+
+# input (JSON): { "users_ids": ["p0", "p1"] }
+# ouput (JSON): { [ { "id": "p0", "latitude": 1, "longitude": 2, "elevation": 3, "distance": 5.25 }, { "id": "p1", "latitude": 4, "longitude": 5, "elevation": 6, "distance": 10.25 } ] }
+# ouput (TEXT): ko 
+# READ MANY
+# devolve a localização de vários pedestres
 @app.route("/getPedestriansByIds", methods=["POST"])
 def getPedestriansByIds():
-    if('users_ids' in request.json):
-        users_ids = request.json.get('users_ids')   #   string json
-        if(len(users_ids) == 0): return "[]"             #   caso tenha enviado a lista vazia
+    if 'users_ids' in request.json:
+        users_ids = request.json['users_ids'] # python object
+        if(len(users_ids) == 0): return "[]" # caso tenha enviado uma lista vazia
         res = "["
         for user_id in users_ids:
-            res += r.get(user_id) + ','
-        res = res[:-1]  #   tirar a última linha
+            if r.exists(user_id):
+                res += r.get(user_id) + ", "
+        if not res.endswith("["): res = res[:-2]
         res += "]"
         return res
-    else: return "ko"
-
-@app.route("/getPedestrians", methods=['GET'])
-def getClosestCrosswalks():
-    keys = r.keys()
-    if not keys: 
-        return "[]"
-    else:
-        res = "["
-        for key in keys:
-            json = r.get(key)
-            res += json
-            res = res[:-2]
-        res += "]"
-        return res
+    else: 
+        return "ko"  # internal server error (é suposto isto não acontecer)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def root():
         return "service is ready"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
